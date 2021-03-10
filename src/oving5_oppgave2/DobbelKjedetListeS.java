7718a5ed34e056140c1dc7a51d20958dbf8178cd
h110541 <110541@stud.hvl.no>
2021-03-10 16:09:37 +0100

    2b), fins()

--- src/oving5_oppgave2/DobbelKjedetListeS.java
+++ src/oving5_oppgave2/DobbelKjedetListeS.java
@@ -1,6 +1,6 @@
 package oving5_oppgave2;
 
-public class DobbelKjedetListeS<T> {
+public class DobbelKjedetListeS<T extends Comparable<T>> {
 
 	private DobbelNode<T> forste;
 	private DobbelNode<T> siste;
@@ -15,6 +15,33 @@ public class DobbelKjedetListeS<T> {
 		siste.setForrige(forste);
 
 		antall = 0;
+	}
+
+	public boolean fins(T element) {
+		sjekkVerdi(element);
+
+		boolean funnet = false;
+		DobbelNode<T> current = forste.getNeste();
+
+		while (element.compareTo(current.getElement()) > 0) {
+			current = current.getNeste();
+		}
+
+		if (element.compareTo(current.getElement()) == 0) {
+			funnet = true;
+		}
+
+		return funnet;
+	}
+
+	// Hjelpemetode, sjekker om en verdi ligger innenfor tillatt område (minVerdi, maksVerdi).
+	// Kaster et unntak dersom verdien er ugyldig.
+	private void sjekkVerdi(T element) {
+		if (element.compareTo(forste.getElement()) <= 0)
+			throw new RuntimeException("Ugyldig verdi (for liten)");
+
+		if (element.compareTo(siste.getElement()) >= 0)
+			throw new RuntimeException("Ugyldig verdi (for stor)");
 	}
 
 }
